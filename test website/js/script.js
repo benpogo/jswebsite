ca02bac61894f67920b214a8b025be4f9e6073fa
Ben <40197081+benpogo@users.noreply.github.com>
2018-06-12 10:03:53 +0000

    Added website files
    added index.html as well as script.js

---
+++ test website/js/script.js
@@ -0,0 +1,27 @@
+let crust = 0;
+let pizza = 0;
+ 
+ function thick () {
+ 	 crust = 1
+ }
+function thin() {
+	crust = 2
+}
+function none() {
+   crust = 3
+}
+function ham() {
+   pizza = 1
+}
+function pepperoni() {
+   pizza = 2
+}
+function pinapple() {
+   pizza = 3
+}
+
+if (crust) = 1 {
+getElementById(pOrder) = 'Thick crust'
+}
+
+console.log(crust)
