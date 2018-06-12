6337a2ddbba67eae20b7c31d1f36cd3aa85dcdfc
Ben Goodwin <ben@goodwin.cc>
2018-06-12 15:51:20 +0000

    new counter added to webpage

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 function dice() {
+var total = 0
 var x = Math.floor(Math.random() * 5 + 1);
-document.getElementById("dOutput").innerHTML = x 	
+document.getElementById("dOutput").innerHTML = x
+ document.getElementById("dTotal").innerHTML = total + x
 }
